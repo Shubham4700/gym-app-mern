bec684d927b99ff2554bb1d31c6957c4788613bc
Shubham4700 <shubhamtan16@gmail.com>
2026-05-05 16:33:41 +0530

    Update project changes

--- frontend/src/components/Programs.jsx
+++ frontend/src/components/Programs.jsx
@@ -69,7 +69,7 @@ const Programs = () => {
       </div>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 overflow-hidden">
 
         {programs.map((item, index) => (
           <motion.div
